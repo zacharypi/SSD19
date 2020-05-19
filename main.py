7802fd7d92d37464a3e032b093d9e04832dab8bf
Zachary Picone <zacharypicone@gmail.com>
2020-05-19 12:11:25 +1200

    added comment

--- main.py
+++ main.py
@@ -27,128 +27,131 @@ from tilemap import *
 
 # HUD functions
 def draw_player_health(surf, x, y, pct):
-    if pct < 0:
-        pct = 0
-    BAR_LENGTH = 100
-    BAR_HEIGHT = 20
-    fill = pct * BAR_LENGTH
-    outline_rect = pg.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
-    fill_rect = pg.Rect(x, y, fill, BAR_HEIGHT)
-    if pct > 0.6:
-        col = GREEN
-    elif pct > 0.4:
-        col = YELLOW
-    else:
-        col = RED
-    pg.draw.rect(surf, col, fill_rect)
-    pg.draw.rect(surf, WHITE, outline_rect, 2)
+	if pct < 0:
+		pct = 0
+	BAR_LENGTH = 100
+	BAR_HEIGHT = 20
+	fill = pct * BAR_LENGTH
+	outline_rect = pg.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+	fill_rect = pg.Rect(x, y, fill, BAR_HEIGHT)
+	if pct > 0.6:
+		col = GREEN
+	elif pct > 0.4:
+		col = YELLOW
+	else:
+		col = RED
+	pg.draw.rect(surf, col, fill_rect)
+	pg.draw.rect(surf, WHITE, outline_rect, 2)
 
 class Game:
-    def __init__(self):
-        pg.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption(TITLE)
-        self.clock = pg.time.Clock()
-        pg.key.set_repeat(500, 100)
-        self.load_data()
+	def __init__(self):
+		pg.init()
+		self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+		pg.display.set_caption(TITLE)
+		self.clock = pg.time.Clock()
+		pg.key.set_repeat(500, 100)
+		self.load_data()
 
-    def load_data(self):
-        game_folder = path.dirname(__file__)
-        img_folder = path.join(game_folder, 'assets')
-        self.map = Map(path.join(game_folder, 'map3.txt'))
-        # Loading in all image assets used
-        self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
-        self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
-        self.fist_img = pg.image.load(path.join(img_folder, FIST_IMG)).convert_alpha()
-        self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
-        self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE)) # Resizes image to correct size
+	def load_data(self):
+		game_folder = path.dirname(__file__)
+		img_folder = path.join(game_folder, 'assets')
+		self.map = Map(path.join(game_folder, 'map3.txt'))
+		# Loading in all image assets used
+		self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+		self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
+		self.fist_img = pg.image.load(path.join(img_folder, FIST_IMG)).convert_alpha()
+		self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
+		self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE)) # Resizes image to correct size
 
-    def new(self):
-        # initialize all variables and do all the setup for a new game
-        self.all_sprites = pg.sprite.Group()
-        self.walls = pg.sprite.Group()
-        self.mobs = pg.sprite.Group()
-        self.fists = pg.sprite.Group()
-        for row, tiles in enumerate(self.map.data):
-            for col, tile in enumerate(tiles):
-                if tile == '1':
-                    Wall(self, col, row)
-                if tile == 'M':
-                    Mob(self, col, row)
-                if tile == 'P':
-                    self.player = Player(self, col, row)
-                self.camera = Camera(self.map.width, self.map.height)
+	def new(self):
+		# initialize all variables and do all the setup for a new game
+		self.all_sprites = pg.sprite.Group()
+		self.walls = pg.sprite.Group()
+		self.mobs = pg.sprite.Group()
+		self.fists = pg.sprite.Group()
+		for row, tiles in enumerate(self.map.data):
+			for col, tile in enumerate(tiles):
+				if tile == '1':
+					Wall(self, col, row)
+				if tile == 'M':
+					Mob(self, col, row)
+				if tile == 'P':
+					self.player = Player(self, col, row)
+				self.camera = Camera(self.map.width, self.map.height)
 
-    def run(self):
-        # game loop - set self.playing = False to end the game
-        self.playing = True
-        while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000.0 # fix for Python 2.x, not needed but still useful
-            self.events()
-            self.update()
-            self.draw()
+	def run(self):
+		# game loop - set self.playing = False to end the game
+		self.playing = True
+		while self.playing:
+			self.dt = self.clock.tick(FPS) / 1000.0 # fix for Python 2.x, not needed but still useful
+			self.events()
+			self.update()
+			self.draw()
 
-    def quit(self):
-        pg.quit()
-        sys.exit()
+	def quit(self):
+		pg.quit()
+		sys.exit()
 
-    def update(self):
-        # update portion of the game loop
-        self.all_sprites.update()
-        self.camera.update(self.player)
-        # mobs hit player // TODO: replace mobs with bullets here
-        hits = pg.sprite.spritecollide(self.player, self.mobs, False, collide_hit_rect)
-        for hit in hits:
-            self.player.health -= MOB_DAMAGE
-            hit.vel = vec(0, 0)
-            if self.player.health <= 0:
-                self.playing = False
-        if hits:
-            self.player.pos += vec(MOB_KNOCKBACK, 0).rotate(-hits[0].rot)
-        # punches hit enemies
-        hits = pg.sprite.groupcollide(self.mobs, self.fists, False, False)
-        for hit in hits:
-            hit.health -= FIST_DAMAGE
-            hit.vel = vec(0, 0)
+	def update(self):
+		# update portion of the game loop
+		self.all_sprites.update()
+		self.camera.update(self.player)
+		# mobs hit player // TODO: replace mobs with bullets here
+		hits = pg.sprite.spritecollide(self.player, self.mobs, False, collide_hit_rect)
+		for hit in hits:
+			self.player.health -= MOB_DAMAGE
+			hit.vel = vec(0, 0)
+			if self.player.health <= 0:
+				self.playing = False
+		if hits:
+			self.player.pos += vec(MOB_KNOCKBACK, 0).rotate(-hits[0].rot)
+		# punches hit enemies
+		hits = pg.sprite.groupcollide(self.mobs, self.fists, False, False)
+		for hit in hits:
+			hit.health -= FIST_DAMAGE
+			hit.vel = vec(0, 0)
 
-    def draw_grid(self): # Draws tile grid over the screen
-        for x in range(0, WIDTH, TILESIZE):
-            pg.draw.line(self.screen, BROWN, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pg.draw.line(self.screen, BROWN, (0, y), (WIDTH, y))
+			# this is utterly fucked
+			# a grave transgression has been made in the creation of this code fuck
 
-    def draw(self):
-        pg.display.set_caption("{:.2f}".format(self.clock.get_fps())) # shows fps in title bar for optimisation purposes
-        self.screen.fill(BGCOLOR)
-        # self.draw_grid() // draws grid over the gameplay area
-        for sprite in self.all_sprites:
-            if isinstance(sprite, Mob):
-                sprite.draw_health()
-            self.screen.blit(sprite.image, self.camera.apply(sprite))
-        # pg.draw.rect(self.screen, WHITE, self.player.hit_rect, 2) // Shows hitbox of player
-        # HUD functions
-        draw_player_health(self.screen, 10, 10, self.player.health / PLAYER_HEALTH)
-        pg.display.flip()
+	def draw_grid(self): # Draws tile grid over the screen
+		for x in range(0, WIDTH, TILESIZE):
+			pg.draw.line(self.screen, BROWN, (x, 0), (x, HEIGHT))
+		for y in range(0, HEIGHT, TILESIZE):
+			pg.draw.line(self.screen, BROWN, (0, y), (WIDTH, y))
 
-    def events(self):
-        # catch all events here
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.quit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    self.quit()
+	def draw(self):
+		pg.display.set_caption("{:.2f}".format(self.clock.get_fps())) # shows fps in title bar for optimisation purposes
+		self.screen.fill(BGCOLOR)
+		# self.draw_grid() // draws grid over the gameplay area
+		for sprite in self.all_sprites:
+			if isinstance(sprite, Mob):
+				sprite.draw_health()
+			self.screen.blit(sprite.image, self.camera.apply(sprite))
+		# pg.draw.rect(self.screen, WHITE, self.player.hit_rect, 2) // Shows hitbox of player
+		# HUD functions
+		draw_player_health(self.screen, 10, 10, self.player.health / PLAYER_HEALTH)
+		pg.display.flip()
 
-    def show_start_screen(self):
-        pass
+	def events(self):
+		# catch all events here
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				self.quit()
+			if event.type == pg.KEYDOWN:
+				if event.key == pg.K_ESCAPE:
+					self.quit()
 
-    def show_go_screen(self):
-        pass
+	def show_start_screen(self):
+		pass
+
+	def show_go_screen(self):
+		pass
 
 # create the game object
 g = Game()
 g.show_start_screen()
 while True:
-    g.new()
-    g.run()
-    g.show_go_screen()
+	g.new()
+	g.run()
+	g.show_go_screen()
